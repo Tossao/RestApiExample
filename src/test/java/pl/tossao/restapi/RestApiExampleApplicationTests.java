ce8ffdcd90362353dcc7b549bf9a0ca7eb0cf990
Tossao <marcinjamroz8@gmail.com>
2020-07-24 23:29:58 +0200

    test

---
+++ src/test/java/pl/tossao/restapi/RestApiExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.tossao.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
