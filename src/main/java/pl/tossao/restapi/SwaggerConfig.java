44ddffb40f7a93dc22314e7f2abdccdc080ea1c4
Tossao <marcinjamroz8@gmail.com>
2020-07-25 00:38:21 +0200

    add SwaggerConfig

---
+++ src/main/java/pl/tossao/restapi/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package pl.tossao.restapi;
+
+import com.fasterxml.classmate.TypeResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.tossao.restapi.util.PageableParameterBuilderPlugin;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.TypeNameExtractor;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Hero Api")
+                .description("Description Hero Api")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("pl.tossao.restapi"))
+                .build();
+    }
+
+    @Bean
+    PageableParameterBuilderPlugin pageableParameterBuilderPlugin(TypeNameExtractor nameExtractor, TypeResolver typeResolver){
+        return new PageableParameterBuilderPlugin(nameExtractor, typeResolver);
+
+    }
+}
