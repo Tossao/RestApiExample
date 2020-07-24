e165ec172ead8347bb2cf501e7b820d8ed50cdd7
Tossao <marcinjamroz8@gmail.com>
2020-07-25 00:37:01 +0200

    add CommandLiner in aplication.java

--- src/main/java/pl/tossao/restapi/RestApiExampleApplication.java
+++ src/main/java/pl/tossao/restapi/RestApiExampleApplication.java
@@ -1,7 +1,14 @@
 package pl.tossao.restapi;
 
+import com.github.javafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import pl.tossao.restapi.model.Hero;
+import pl.tossao.restapi.repository.HeroRepository;
+
+import java.util.stream.IntStream;
 
 @SpringBootApplication
 public class RestApiExampleApplication {
@@ -10,4 +17,10 @@ public class RestApiExampleApplication {
 		SpringApplication.run(RestApiExampleApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner commandLineRunner(HeroRepository heroRepository) {
+		Faker faker = new Faker();
+		return args -> IntStream.range(0,100).forEach(i->heroRepository.save(new Hero(faker.superhero().name())));
+
+	}
 }
